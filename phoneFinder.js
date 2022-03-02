efef5d78df46cc2700f5f13b7d5b818a5c921b73
tusher112 <mdtusherahmed820@gmail.com>
2022-03-02 20:30:55 +0600

    repeat photo problem solve and details added

--- phoneFinder.js
+++ phoneFinder.js
@@ -14,19 +14,20 @@ const searchPhone = () => {
 const displaySearchResult = phones => {
     // console.log(phones);
     const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
     phones.forEach(phone => {
         console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="col">
-                <div class="card h-100">
+        <div class="col col-sm-12 ">
+                <div class="card h-100 ">
                     <img src="${phone.image}" class="card-img-top p-5" alt="...">
                     <div class="card-body text-center">
                         <h5 class="card-title">${phone.phone_name}</h5>
                         <h5 class="card-title">Brand: ${phone.brand}</h5>
-                        <button  type="button"
-                        class="btn btn-outline-primary">More details</button>
+                        <button onclick="loadPhoneDetail('${phone.slug}')" type="button"
+                        class="btn btn-outline-primary">More details </button>
                     </div>
                 </div>
             </div>
@@ -34,4 +35,37 @@ const displaySearchResult = phones => {
         searchResult.appendChild(div);
 
     });
+}
+
+const loadPhoneDetail = phoneId => {
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => DisplayPhoneDetails(data.data));
+
+}
+
+const DisplayPhoneDetails = phone => {
+    const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+                    <img src="${phone.image}" class="card-img-top p-5" alt="...">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">${phone.name}</h5>
+                        <h5 class="card-title">Brand: ${phone.brand}</h5>
+                        <h6 class="card-title">Release Date: ${phone.releaseDate}</h6>
+                        <h6 class="card-title">ChipSet: ${phone.mainFeatures.chipSet}</h6>
+                        <h6 class="card-title">Size: ${phone.mainFeatures.displaySize}</h6>
+                        <h6 class="card-title">Memory: ${phone.mainFeatures.memory}</h6>
+                        <h6 class="card-title">Storage: ${phone.mainFeatures.storage}</h6>
+                        <button  type="button"
+                        class="btn btn-outline-primary">More details</button>
+                    </div>
+                </div>
+            </div>
+        `;
+    phoneDetails.appendChild(div);
+
 }
